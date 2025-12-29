facc8592423863055c5d7a1d3d57c35f496eeb1d
Hardik Ajmeriya <hardikpt95@gmail.com>
2025-12-30 01:38:43 +0530

    chore: config the backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,24 +3,36 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import path from "path";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 const __dirname = path.resolve();
 
+// middlewares
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
+// routes
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
-//make ready for deployment
+// production frontend serve
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
-app.listen(3000, () => console.log("Server is running on port: " + PORT));
+app.listen(PORT, () => {
+  console.log("Server is running on port: " + PORT);
+});
