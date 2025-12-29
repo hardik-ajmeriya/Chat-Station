ae702a8e3f04cfd9da1b05c3d99cb5f36ab81f6e
Hardik Ajmeriya <hardikpt95@gmail.com>
2025-12-29 23:11:49 +0530

    feat: frontend & backend Combine

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,14 +2,25 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
+
+//make ready for deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(3000, () => console.log("Server is running on port: " + PORT));
