f8d70821505483fe5b701f76cd2b4f39594ec14d
Hardik Ajmeriya <hardikpt95@gmail.com>
2025-12-30 22:54:07 +0530

    feat: Welcome-Email Integrated

---
+++ backend/src/emails/emailHandlers.js
@@ -0,0 +1,18 @@
+import { resendClient, sender } from "../lib/resend.js";
+import { createWelcomeEmailTemplate } from "../emails/emailTamplates.js";
+
+export const sendWelcomeEmail = async (email, name, clientURL) => {
+  const { data, error } = await resendClient.emails.send({
+    from: `${sender.name} <${sender.email}>`,
+    to: email,
+    subject: "Welcome to chat-station-app!",
+    html: createWelcomeEmailTemplate(name, clientURL),
+  });
+
+  if (error) {
+    console.error("Error sending welcome email:", error);
+    throw new Error("Failed to send welcome email");
+  }
+
+  console.log("Welcome Email sent successfully", data);
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,13 @@
 import express from "express";
-import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import cors from "cors";
 import { connectDB } from "./lib/db.js";
+import { ENV } from "./lib/env.js";
 
-dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = ENV.PORT || 3000;
 
 // middlewares
 app.use(cors({ origin: "*" }));
