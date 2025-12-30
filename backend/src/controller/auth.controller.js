c80fd0797a6e66f192a69851819cbb8463d94827
Hardik Ajmeriya <hardikpt95@gmail.com>
2025-12-30 19:23:32 +0530

    chore: Backend files

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -36,7 +36,10 @@ export const signup = async (req, res) => {
     });
 
     if (newUser) {
-      generateToken(newUser._id, res);
+      //generateToken(newUser._id, res);
+
+      const savedUser = await newUser.save();
+      generateToken(savedUser._id, res);
 
       res.status(201).json({
         _id: newUser._id,
