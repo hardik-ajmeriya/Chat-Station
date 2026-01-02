4a94860ef1b65815947363a0e4e4838cd2cca1d0
Hardik Ajmeriya <hardikpt95@gmail.com>
2026-01-02 06:04:03 +0530

    signup-module Completed

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -9,15 +9,11 @@ import cloudinary from "../lib/cloudinary.js";
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
 
-  if (!email || !password) {
-    return res.status(400).json({ message: "Email and Password are required" });
-  }
-
   try {
     if (!fullName || !email || !password) {
       return res.status(400).json({ message: "All fields are required" });
     }
-
+    
     if (password.length < 6) {
       return res
         .status(400)
