4d6d6594d4af7fe06012f45c5aaa85b299ffeb97
Hardik Ajmeriya <hardikpt95@gmail.com>
2026-01-02 06:24:28 +0530

    feat: vercel fileV1.2

--- frontend/src/lib/axios.jsx
+++ frontend/src/lib/axios.jsx
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+const baseURL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3000/api"
+    : import.meta.env.VITE_API_URL || "/api";
+
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:3000/api"
-      : "/api",
+  baseURL: baseURL,
   withCredentials: true,
 });
