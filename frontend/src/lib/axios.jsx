b9a68593373018f189b309a6d2969d5478029d36
Hardik Ajmeriya <hardikpt95@gmail.com>
2026-01-02 06:10:46 +0530

    fix: signup-module

--- frontend/src/lib/axios.jsx
+++ frontend/src/lib/axios.jsx
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api", // 🔥 FORCE LOCAL
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:3000/api"
+      : "/api", // 🔥 FORCE LOCAL
   withCredentials: true,
 });
