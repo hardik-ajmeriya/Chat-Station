97cd27b11695f11a5e2f704665c7f0a3780d5621
Hardik Ajmeriya <hardikpt95@gmail.com>
2026-01-03 23:12:45 +0530

    fix: bugsv1.1

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,7 +4,9 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3000"
+    : "https://chat-station.onrender.com"; // Render backend origin for production
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
